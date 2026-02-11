bfa880102994b6e1b76e697c1565e1763ce5f64e
LuxZen-Tao <22331820@bucks.ac.uk>
2026-02-11 01:01:42 +0000

    Implement VIP arc consequences with narrative delivery

---
+++ FeatureFlags.java
@@ -0,0 +1,8 @@
+public final class FeatureFlags {
+    private FeatureFlags() {
+    }
+
+    public static boolean FEATURE_SEASONS = false;
+    public static boolean FEATURE_RIVALS = false;
+    public static boolean FEATURE_VIPS = false;
+}
